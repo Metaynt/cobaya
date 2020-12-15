ca0446dc728ae75ec054f97e6a3b60e005491b9e
Metaynt <72543445+Metaynt@users.noreply.github.com>
2020-12-15 22:50:52 +0700

    Add files via upload

---
+++ qgis2web_2020_12_15-22_35_47_608618/data/cobawedgis_1.js
@@ -0,0 +1,8 @@
+var json_cobawedgis_1 = {
+"type": "FeatureCollection",
+"name": "cobawedgis_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.831219137571168, -6.185329300481762 ] } }
+]
+}
